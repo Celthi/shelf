6eb228bb72d687626ec7d4c1accb6d19ba539831
Celthi <txtlu0@gmail.com>
2015-10-17 21:46:18 +0800

    add string cat

---
+++ my_strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define LENGTH
+
+void my_strcat(char *s, char *t);
+
+int main(int argc, char *argv[])
+{
+	char s[100] = "we are ";
+	char t[10] = "good";
+	my_strcat(s, t);
+	printf("%s\n", s);
+	
+
+	return 0;
+
+}
+
+void my_strcat(char *s, char *t)
+{
+	while (*s++)
+		;
+	s--;
+	while (*s++ = *t++)
+		;
+}
